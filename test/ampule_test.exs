ea1ad26cc8ee52537e34d1b0ee01612b4ae2cc96
Michael Santos <michael.santos@gmail.com>
2014-02-22 09:46:22 -0500

    Add some tests

--- test/ampule_test.exs
+++ test/ampule_test.exs
@@ -1,7 +1,15 @@
 defmodule AmpuleTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "anonymous container" do
+    result = {:lists, :reverse, ['1234567890']} |> Ampule.call
+    assert('0987654321' == result)
   end
+
+  test "named container" do
+    container = Ampule.spawn
+    result = {:lists, :reverse, ['1234567890']} |> Ampule.call(container)
+    assert('0987654321' == result)
+  end
+
 end
